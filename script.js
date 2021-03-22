199b0b72cd540b39ccd0f23587ce7f12d2e51cf3
patrickhaussmann <git@patrickhaussmann.de>
2021-03-22 14:37:54 +0100

    link to github source

--- script.js
+++ script.js
@@ -327,7 +327,8 @@ function draw(geojson) {
             '<a href="https://github.com/owid/covid-19-data/tree/master/public/data" target="_blank">OWID</a>, ' +
             '<a href="https://npgeo-corona-npgeo-de.hub.arcgis.com/datasets/dd4580c810204019a7b8eb3e0b329dd6_0/data" target="_blank">RKI</a>, ' +
             '<a href="https://npgeo-corona-npgeo-de.hub.arcgis.com/datasets/ef4b445a53c1406892257fe63129a8ea_0/data" target="_blank">RKI</a>, ' +
-            '<a href="https://npgeo-corona-npgeo-de.hub.arcgis.com/datasets/917fc37a709542548cc3be077a786c17_0/data" target="_blank">DIVI</a>',
+            '<a href="https://npgeo-corona-npgeo-de.hub.arcgis.com/datasets/917fc37a709542548cc3be077a786c17_0/data" target="_blank">DIVI</a>, ' +
+            '<a href="https://github.com/PatrickHaussmann/api.corona.app.5ls.de" target="_blank">source</a>',
     });
     Layer.addTo(map);
 
