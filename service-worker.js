60154443efd6e350b3f59e98d2c615c58374027d
patrickhaussmann <git@patrickhaussmann.de>
2021-08-26 13:57:46 +0200

    Improve Caching

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,33 @@
 importScripts(
   "https://storage.googleapis.com/workbox-cdn/releases/6.2.4/workbox-sw.js"
 );
+const { StaleWhileRevalidate, CacheFirst, NetworkFirst } = workbox.strategies;
 const { registerRoute, setDefaultHandler } = workbox.routing;
-const { StaleWhileRevalidate, CacheFirst } = workbox.strategies;
 const { cacheNames, setCacheNameDetails } = workbox.core;
-const { ExpirationPlugin } = workbox.expiration;
 const { CacheableResponsePlugin } = workbox.cacheableResponse;
+const { ExpirationPlugin } = workbox.expiration;
 
-setCacheNameDetails({ suffix: "v1" });
+setCacheNameDetails({ suffix: "v2" });
 cacheNames.expiration = cacheNames.prefix + "-expiration-" + cacheNames.suffix;
+cacheNames.offline = cacheNames.prefix + "-offline-" + cacheNames.suffix;
+cacheNames.network = cacheNames.prefix + "-network-" + cacheNames.suffix;
+cacheNames.stale = cacheNames.prefix + "-stale-" + cacheNames.suffix;
 
 registerRoute(
   ({ url }) =>
     ["https://cdn.jsdelivr.net"].includes(url.origin) ||
-    url.href.endsWith(".geo.json"),
-  new CacheFirst()
+    (url.origin == location.origin &&
+      new RegExp("\\.(json|svg|png|geo.json)$").test(url.pathname)),
+  new CacheFirst({
+    cacheName: cacheNames.offline,
+  })
 );
 
 registerRoute(
   ({ url }) => url.origin == location.origin,
-  new StaleWhileRevalidate()
+  new StaleWhileRevalidate({
+    cacheName: cacheNames.stale,
+  })
 );
 
 registerRoute(
@@ -40,40 +48,17 @@ registerRoute(
 );
 
 setDefaultHandler(
-  new StaleWhileRevalidate({
-    cacheName: cacheNames.expiration,
-    plugins: [
-      new ExpirationPlugin({
-        maxAgeSeconds: 1 * 60 * 60,
-        matchOptions: { ignoreVary: true },
-      }),
-    ],
+  new NetworkFirst({
+    cacheName: cacheNames.network,
   })
 );
 
 self.addEventListener("install", (event) => {
-  const urls = [
-    "/",
-    "/404",
-    "/config.js",
-    "/script.js",
-    "/style.css",
-    "/graph",
-    "/graph.js",
-    "/back.svg",
-    "/districts.geo.json",
-    "/states.geo.json",
-    "/world.geo.json",
-    "/manifest.json",
-    "/favicon.ico",
-    "https://cdn.jsdelivr.net/npm/leaflet@1.7.1/dist/leaflet.min.css",
-    "https://cdn.jsdelivr.net/npm/leaflet@1.7.1/dist/leaflet.min.js",
-    "https://cdn.jsdelivr.net/npm/redom@3.27.1/dist/redom.min.js",
-    "https://cdn.jsdelivr.net/npm/highcharts@9.2.1/highcharts.min.js",
-    "https://cdn.jsdelivr.net/npm/charts.css@0.9.0/dist/charts.min.css",
-  ];
-  const cacheName = cacheNames.runtime;
-  event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(urls)));
+  event.waitUntil(
+    caches
+      .open(cacheNames.stale)
+      .then((cache) => cache.addAll(["/", "/404", "/graph"]))
+  );
 });
 
 self.addEventListener("activate", (event) => {
